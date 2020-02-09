595a939434cefbe27913953d49ebb061e3fa10a6
Zak8416 <37023607+Zak8416@users.noreply.github.com>
2020-02-09 15:36:38 +0200

    Update Icarus.cs

--- Icarus.cs
+++ Icarus.cs
@@ -1,32 +1,51 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp8
+namespace ConsoleApp6
 {
     class Program
     {
         static void Main(string[] args)
         {
-           int n = int.Parse(Console.ReadLine());
-           int[] a = new int[n];
-            // поемане
-            for (int i = 0; i < a.Length; i++)
+            char[] splitters = {' '};
+            string[] s = Console.ReadLine().Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+            int n = s.Length;
+            int[] plane = new int[n];
+
+            for (int i = 0; i < n; i++)
+                plane[i] = int.Parse(s[i]);
+
+            int CurrPos = int.Parse(Console.ReadLine());
+            int damage = 1;
+
+            string[] command;
+            string direction = "";
+            int steps = 0;
+
+            do
             {
-               a[i] = int.Parse(Console.ReadLine());
+                command = Console.ReadLine().Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+                if (command.Length > 1)
+                {
+                    direction = command[0];
+                    steps = int.Parse(command[1]);
+                }
+
+                // processing commands
+                if (direction == "left")
+                {
+                    CurrPos -= steps % n;
+                    damage += steps / n;
+                }
+
+
             }
+            while (command[0] != "Supernova");
 
-            Array.Sort(a);
-
-            int x = n < 3 ? n : 3;
-
-            // извеждане
-            for (int i = 0; i < x; i++)
-            {
-                Console.WriteLine(a[i]);
-            }
+            Console.WriteLine();
         }
     }
 }
