34f7a5dcf9cbf02794044a801e87f116b05df4c3
Zak8416 <37023607+Zak8416@users.noreply.github.com>
2020-02-09 15:34:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+           int[] a = new int[n];
+            // поемане
+            for (int i = 0; i < a.Length; i++)
+            {
+               a[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(a);
+
+            int x = n < 3 ? n : 3;
+
+            // извеждане
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
